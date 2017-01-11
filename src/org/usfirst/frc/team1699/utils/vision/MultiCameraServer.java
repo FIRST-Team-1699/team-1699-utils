df91fdef939b97798ed62fa58456bc2ccb61aa66
Connor <thatging3rkid@users.noreply.github.com>
2017-01-11 17:08:32 -0500

    updated to wpilib 2017.1

--- src/org/usfirst/frc/team1699/utils/vision/MultiCameraServer.java
+++ src/org/usfirst/frc/team1699/utils/vision/MultiCameraServer.java
@@ -12,53 +12,10 @@
  */
 package org.usfirst.frc.team1699.utils.vision;
 
-import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.Image;
 
-import edu.wpi.first.wpilibj.CameraServer;
-
+@Deprecated
 public class MultiCameraServer {
-	
-	private int _session;
-	private Image frame;
-	private int _cameraCount;
-	private int selectedCamera = 0;
-	private boolean firstTime = true;
-	
-	public MultiCameraServer(int camCount){
-		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-		_cameraCount = camCount;
-	}
-	
-	public void run(){
-		NIVision.IMAQdxGrab(_session, frame, 1);
-		CameraServer.getInstance().setImage(frame);
-	}
-	
-	public void setCamera(int cam){
-		if (cam >= _cameraCount || cam < 0){
-			System.out.println("Error: tried to set camera out of bounds.");
-			return;
-		}
-		
-		if(!firstTime){
-			NIVision.IMAQdxStopAcquisition(_session);
-			NIVision.IMAQdxCloseCamera(_session);
-		}else{
-			firstTime = false;
-		}
-		
-		selectedCamera = cam;
-		_session = NIVision.IMAQdxOpenCamera("cam" + selectedCamera, NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-		NIVision.IMAQdxConfigureGrab(_session);
-		NIVision.IMAQdxStartAcquisition(_session);
-	}
-	
-	public int getCamera() {
-		return selectedCamera;
-	}
-	
-	public void toggleCamera() {
-		setCamera((selectedCamera + 1) % _cameraCount);
-	}
+	/*
+	 * NI Vision was taken out in WPILib 2017.1
+	 */
 }
