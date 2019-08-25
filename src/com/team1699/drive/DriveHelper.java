9321fb551f4acc28de4f1e989d0f2fd9a3f2b44b
Jakob Misbach <jakobmisbach8@gmail.com>
2019-08-25 01:09:13 -0400

    Worked on drive code

--- src/com/team1699/drive/DriveHelper.java
+++ src/com/team1699/drive/DriveHelper.java
@@ -3,4 +3,7 @@ package com.team1699.drive;
 public class DriveHelper {
 
     //A class that will contain a helper method with the math needed to generate a drive signal from joystick inputs
+    public DriveSignal getDrive(double throttle, double turn, boolean quickTurn){
+        return null;
+    }
 }
--- src/com/team1699/drive/DriveSignal.java
+++ src/com/team1699/drive/DriveSignal.java
@@ -1,6 +1,42 @@
 package com.team1699.drive;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class DriveSignal {
 
     //A class that will represent the signal that will be send to the drive train
+    private final double mPortMotor;
+    private final double mStarMotor;
+    private final boolean mBrakeMode;
+
+    public DriveSignal(final double portMotor, final double starMotor){
+        this(portMotor, starMotor, false);
+    }
+
+    public DriveSignal(final double portMotor, final double starMotor, final boolean brakeMode){
+        this.mPortMotor = portMotor;
+        this.mStarMotor = starMotor;
+        this.mBrakeMode = brakeMode;
+    }
+
+    public static DriveSignal fromControls(double throttle, double turn){
+        return new DriveSignal(throttle - turn, throttle + turn);
+    }
+
+    public double getPortMotor() {
+        return mPortMotor;
+    }
+
+    public double getStar() {
+        return mStarMotor;
+    }
+
+    public boolean getBrakeMode() {
+        return mBrakeMode;
+    }
+
+    @Override
+    public String toString() {
+        return "Port: " + mPortMotor + ", Star: " + mStarMotor + (mBrakeMode ? ", BRAKE" : "");
+    }
 }
